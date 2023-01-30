377a09443c237006513f1247abc5c347190eaf33
Gábor Hajdu <gabor.hajdu@codecool.com>
2023-01-30 16:06:06 +0100

    add people endpoints

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const people = [
+    {
+        name: 'Béla',
+        birthYear: 1990
+    },
+    {
+        name: 'Lajos',
+        birthYear: 1991
+    }
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -10,10 +21,20 @@ const szia2Handler = (req, res) => {
     // res.send(`Szia ${req.query.name == null ? 'világ' : req.query.name}!`)
     // res.send(`Szia ${req.query.name || 'világ'}!`)
     res.send(`Hello ${req.params.name || 'világ'}!`)
-};
+}
 
 app.get('/szia2', szia2Handler)
 app.get('/szia2/:name', szia2Handler)
+
+// not proper, if you check content-type response header in the browser, you'll see text/html
+app.get('/people', (req, res) => {
+    res.send(JSON.stringify(people))
+})
+
+// with proper response header
+app.get('/people2', (req, res) => {
+    res.json(people)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
